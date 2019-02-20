a8f19154378ee466613509597dc93fae036bd177
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-02-19 19:12:21 -0600

    Avances variados en css y html

--- src/data.js
+++ src/data.js
@@ -6,3 +6,9 @@ const example = () => {
 };
 
 window.example = example;
+
+
+const motoAccident = INJURIES.filter(moto => (moto.Total_Injured_Persons_Motorcyclists));
+console.log(motoAccident); //esto nos mostro todos los arreglos de la base de datos pero no especificamente los accidentes de moto 
+
+
