b19330073fb155287c35a7cb280aea6571871b2b
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-02-20 11:59:39 -0600

    cambios andrea

--- src/data.js
+++ src/data.js
@@ -4,11 +4,39 @@
 const example = () => {
   return 'example';
 };
-
 window.example = example;
+window.datalover = {
+  // meter aqui el nombre de las funciones que ocupemos
+};
+showMoto: function (){
+        let listAccidentMoto=[]; //este es el arreglo vacio donde vamos a guardar la cantidad en # de accidentes        
+        let listAccidentMotoYear=[]; //este arreglo vacio es donde se guardan los años en los que aparece los accidentes de moto
+        for(let i=0;i<INJURIES.length; i++){ // este ciclo for cuenta a lo largo de todo INJURIES 
+        let accidentMoto=INJURIES[i].Total_Injured_Persons_Motorcyclists; //le pedimos aquí todos los Total_Injured_Persons_Motorcyclists
+        let accidentMotoYear= INJURIES[i].Year; //le pedimos aquí todos los Year  
+        listAccidentMoto.push(accidentMoto); //llena el arreglo "listAccidentMoto" con la variable "accidentMoto"
+        listAccidentMotoYear.push(accidentMotoYear); //llena el arreglo "listAccidentMotoYear" con la variable "accidentMotoYear"
+}
 
+};
 
-const motoAccident = INJURIES.filter(moto => (moto.Total_Injured_Persons_Motorcyclists));
-console.log(motoAccident); //esto nos mostro todos los arreglos de la base de datos pero no especificamente los accidentes de moto 
+let listAccidentBike=[];
+// ejemplo que nos dio ANA
+// listAccidentBike.forEach(elemento=>console.log(elemento));
 
+let listAccidentBikeYear=[];
+for(let i=0;i<INJURIES.length; i++){
+let accidentBike=INJURIES[i].Total_Injured_Persons_Pedalcyclists;
+let accidentBikeYear= INJURIES[i].Year;
+listAccidentBike.push(accidentBike);
+listAccidentBikeYear.push(accidentBikeYear);
+};
 
+let listAccidentCar=[];
+let listAccidentCarYear=[];
+for(let i=0;i<INJURIES.length; i++){
+let accidentCar=INJURIES[i].Total_Injured_Persons_Passenger_Car_Occupants;
+let accidentCarYear= INJURIES[i].Year;
+listAccidentCar.push(accidentCar);
+listAccidentCarYear.push(accidentCarYear);
+};
