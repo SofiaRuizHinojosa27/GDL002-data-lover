6bf31b27f6870f2750df9535523dfa2f39589dda
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-02-21 08:23:38 -0600

    Se hicieron ajustes a CSS

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 
 function motoAccidents(){
-
-  let listMoto=window.datalover.motoAccident;
-  //console.log(listMoto);
-  document.getElementById("Results").value= listMoto;
+  let listMoto=window.datalover.listAccidentMoto;
+  listAccidentMotoYear.forEach((element)=>{
+     document.getElementById("ResultsYear").value += element + "\n";
+  });
   //document.getElementById("ResultsYear").value=listAccidentMotoYear;
-}
-
+   document.getElementById("Results").value = listAccidentMoto; 
+  }
 document.getElementById("btnMoto").addEventListener('click',motoAccidents);
 
 function bikeAccidents(){
@@ -22,16 +22,3 @@ function carAccidents(){
   document.getElementById("Results").value=listAccidentCar;
   }
 document.getElementById("btnCar").addEventListener('click',carAccidents);
-
-function clean(){
-  document.getElementById("ResultsYear").value="";
-  document.getElementById("Results").value="";
-}
-document.getElementById("Clean").addEventListener('click',clean);
-
-// function anualData(){
-//   document.getElementById("pag1").style.display="none";
-//   document.getElementById("AnualData").style.display="block";
-// }
-// document.getElementById("btnAnual").addEventListener('click',anualData);
-
