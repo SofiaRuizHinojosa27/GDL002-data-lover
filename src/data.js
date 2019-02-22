fb5f6fbc504431c6a7cbdb8de0f1655cedee93ed
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-02-22 08:38:19 -0600

    Se hizo rediseño en CSS y funcion mostrado de seccion de datos anuales

--- src/data.js
+++ src/data.js
@@ -1,42 +1,56 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
 const example = () => {
   return 'example';
 };
 window.example = example;
 window.datalover = {
   // meter aqui el nombre de las funciones que ocupemos
+  motoAccident:motoAccident(),
+  bikeAccident:bikeAccident(),
+  carAccident:carAccident(),
+  yearAccident:yearAccident()
 
 };
 
+function motoAccident(){
+    let listAccidentMoto=[];//nos dara el numero de accidentes ocurridos por año
 
-let listAccidentMoto=[]; //este es el arreglo vacio donde vamos a guardar la cantidad en # de accidentes        
-let listAccidentMotoYear=[]; //este arreglo vacio es donde se guardan los años en los que aparece los accidentes de moto
-for(let i=0;i<INJURIES.length; i++){ // este ciclo for cuenta a lo largo de todo INJURIES 
-let accidentMoto=INJURIES[i].Total_Injured_Persons_Motorcyclists; //le pedimos aquí todos los Total_Injured_Persons_Motorcyclists
-let accidentMotoYear= INJURIES[i].Year; //le pedimos aquí todos los Year  
-listAccidentMoto.push(accidentMoto); //llena el arreglo "listAccidentMoto" con la variable accidentMoto"
-listAccidentMotoYear.push(accidentMotoYear);
-};
+    for(let i=0;i<INJURIES.length; i++){//contara el numero de arreglos existentes en nuestra data
+      let accidentMoto=INJURIES[i].Total_Injured_Persons_Motorcyclists;//realizara un valor de arreglo por cada accidente de moto
+      listAccidentMoto.push(accidentMoto);//subir a nuestra lista de accidentes de motos, los arreglos encontrados
+  }
+  // // ejemplo que nos dio ANA
+  // // listAccidentBike.forEach(element=>console.log(element));
+  return listAccidentMoto;
+}
 
-let listAccidentBike=[];
-// ejemplo que nos dio ANA
-// listAccidentBike.forEach(elemento=>console.log(elemento));
+function bikeAccident(){
+  let listAccidentBike=[];
+  // // ejemplo que nos dio ANA
+  // // listAccidentBike.forEach(element=>console.log(element));
+  // let listAccidentBikeYear=[];
+  for(let i=0;i<INJURIES.length; i++){
+    let accidentBike=INJURIES[i].Total_Injured_Persons_Pedalcyclists;
+    listAccidentBike.push(accidentBike);
+}
+ return listAccidentBike;
+}
 
-let listAccidentBikeYear=[];
-for(let i=0;i<INJURIES.length; i++){
-let accidentBike=INJURIES[i].Total_Injured_Persons_Pedalcyclists;
-let accidentBikeYear= INJURIES[i].Year;
-listAccidentBike.push(accidentBike);
-listAccidentBikeYear.push(accidentBikeYear);
-};
 
-let listAccidentCar=[];
-let listAccidentCarYear=[];   //HSR: El error estaba aquí, se fue un espacio de más y marcaba un error de sintaxis. :*
-for(let i=0;i<INJURIES.length; i++){
-let accidentCar=INJURIES[i].Total_Injured_Persons_Passenger_Car_Occupants;
-let accidentCarYear= INJURIES[i].Year;
-listAccidentCar.push(accidentCar);
-listAccidentCarYear.push(accidentCarYear);
-};
+function carAccident(){
+  let listAccidentCar=[];
+  for(let i=0;i<INJURIES.length; i++){
+    let accidentCar=INJURIES[i].Total_Injured_Persons_Passenger_Car_Occupants;
+    listAccidentCar.push(accidentCar);
+  }
+  return listAccidentCar;
+}
+
+function yearAccident(){
+  let yearTime=[];//nos dara el numero de accidentes ocurridos por año
+
+  for(let i=0;i<INJURIES.length; i++){
+    let accidentYear=INJURIES[i].Year;
+    yearTime.push(accidentYear);
+}
+return yearTime;
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
+function showAnualData(){
+document.getElementById("AnualData").style.display = 'block';
+}
+document.getElementById("btnAnual").addEventListener('click',showAnualData);
+
+function time(){
+  let yearAccident=window.datalover.yearAccident;
+yearAccident.forEach(element=>document.getElementById("ResultsYear").value += element + "\n");
+}
+
+document.getElementById("btnMoto").addEventListener('click',time);
+document.getElementById("btnBike").addEventListener('click',time);
+document.getElementById("btnCar").addEventListener('click',time);
 
 function motoAccidents(){
-  let listMoto=window.datalover.listAccidentMoto;
-  listAccidentMotoYear.forEach((element)=>{
-     document.getElementById("ResultsYear").value += element + "\n";
-  });
-  //document.getElementById("ResultsYear").value=listAccidentMotoYear;
-   document.getElementById("Results").value = listAccidentMoto; 
-  }
+  let listMoto=window.datalover.motoAccident;
+listMoto.forEach(element=>document.getElementById("Results").value += element + "\n");
+}
 document.getElementById("btnMoto").addEventListener('click',motoAccidents);
 
 function bikeAccidents(){
-  let listBike=window.datalover.listAccidentBike;
-  document.getElementById("ResultsYear").value=listAccidentBikeYear;
-  document.getElementById("Results").value=listAccidentBike;
-  }
+  let listBike=window.datalover.bikeAccident;
+  listBike.forEach(element=>document.getElementById("Results").value += element + "\n");
+}
 document.getElementById("btnBike").addEventListener('click',bikeAccidents);
 
 function carAccidents(){
-  let listBike=window.datalover.listAccidentCar;
-  document.getElementById("ResultsYear").value=listAccidentCarYear;
-  document.getElementById("Results").value=listAccidentCar;
-  }
+  let listCar=window.datalover.carAccident;
+  listCar.forEach(element=>document.getElementById("Results").value += element + "\n");
+}
 document.getElementById("btnCar").addEventListener('click',carAccidents);
+
+function clean(){
+  document.getElementById("ResultsYear").value= "";
+  document.getElementById("Results").value= "";
+}
+document.getElementById("Clean").addEventListener('click',clean);
