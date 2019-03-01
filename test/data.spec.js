06a8872c8e0e52b2a4ef9354826716a36fd531c5
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-03-01 13:37:01 -0600

    se agrega estilo final y html

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,17 @@
 require('../src/data.js');
+const INJURIES= require(../src/injuries/injuries.json);
 
-const injuries = require('../src/data/injuries/injuries.json');
+describe('INJURIES', () => {
+  it('should be an object', () => {
+    expect(typeof INJURIES).toBe('object');
+});
+});
 
-
-describe('accidenteMoto', () => {
-  it('should be an array', () => {
-    expect(typeof injuries).toBe('array');
-  });
+describe('function yearAccident', () = > {
+  it('should return yearAccident 1960', () =>{
+    expect(window.datalover.yearAccident(INJURIES)[0]).toEqual({"Automovil":"sin datos", "Bicicleta":"sin datos", })
+  })
+})
 
 //  it('returns `example`', () => {
  //   expect(example()).toBe('example');
